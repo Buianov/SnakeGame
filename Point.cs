24150ec123f2d77f1e1fa8878a8952f09ff1568f
Buianov <buianov@mail.ru>
2020-11-05 18:07:52 +0000

    Added Point class

---
+++ Point.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Snake {
+  class Point {
+    public int x;
+    public int y;
+    public char sym;
+
+    public void Draw() {
+      Console.SetCursorPosition(x, y);
+      Console.Write(sym);
+    }
+  }
+}
--- main.cs
+++ main.cs
@@ -1,7 +1,16 @@
 using System;
 
-class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+namespace Snake {
+  class Program {
+    public static void Main (string[] args) {
+      
+      Point p1 = new Point();
+      p1.x = 5;
+      p1.y = 5;
+      p1.sym = '^';
+      p1.Draw();
+
+      Console.ReadLine();
+    }
   }
 }
